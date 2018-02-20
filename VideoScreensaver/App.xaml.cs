0f0a5ece9a0413e2c6fdd688df0166af6d622391
Chris Brown <chris@chrisbrown.id.au>
2018-02-20 22:17:42 +1100

    cleanup of usings

--- VideoScreensaver/App.xaml.cs
+++ VideoScreensaver/App.xaml.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Forms;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
 
 namespace VideoScreensaver
 {
